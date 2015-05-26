b22a408a032135220823a08e7239acfb2aa017ef
Juan Sebastian Vera <sebastianvera@ubuntu.(none)>
2015-05-26 20:32:05 -0300

    migration

--- Toolchain/ModulePacker/modulePacker.h
+++ Toolchain/ModulePacker/modulePacker.h
@@ -1,7 +1,7 @@
 #ifndef _MODULE_PACKER_H_
 #define _MODULE_PACKER_H_
 
-#define LOG(_fmt, ...) fprintf(stderr, _fmt, __VA_ARGS__ )
+#include <argp.h>
 
 #define FALSE 0
 #define TRUE !FALSE
@@ -10,13 +10,21 @@
 
 #define OUTPUT_FILE "packedKernel.bin"
 
+#define MAX_FILES 128
 
 typedef struct {
 	char **array;
 	int length;
 } array_t;
 
-
+/* Used by main to communicate with parse_opt. */
+struct arguments
+{
+  char *args[MAX_FILES];                
+  int silent, verbose;
+  char *output_file;
+  int count;
+};
 
 
 int write_size(FILE *target, char *filename);
@@ -25,5 +33,7 @@ int write_file(FILE *target, FILE *source);
 
 int checkFiles(array_t fileArray);
 
+static error_t
+parse_opt (int key, char *arg, struct argp_state *state);
 
 #endif
